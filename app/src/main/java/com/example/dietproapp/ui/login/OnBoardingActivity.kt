88c5b6be21bb3926a26fd868111b1eaf38028d2a
agaaaptr <gagah.p.412@gmail.com>
2023-07-26 14:44:21 +0700

    style(theme): Tema

--- app/src/main/java/com/example/dietproapp/ui/login/OnBoardingActivity.kt
+++ app/src/main/java/com/example/dietproapp/ui/login/OnBoardingActivity.kt
@@ -3,7 +3,10 @@ package com.example.dietproapp.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import com.example.dietproapp.R
 import com.example.dietproapp.databinding.ActivityOnboardingBinding
 import com.inyongtisto.myhelper.extension.intentActivity
@@ -19,6 +22,12 @@ class OnBoardingActivity : AppCompatActivity() {
         _binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
+        title = "DietProApp"
+        val window: Window = this@OnBoardingActivity.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = ContextCompat.getColor(this@OnBoardingActivity, R.color.primary)
 
         startButton()
 
