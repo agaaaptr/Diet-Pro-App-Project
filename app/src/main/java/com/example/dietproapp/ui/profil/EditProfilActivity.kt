589734ffe1d307859830c0a6b1621006aca465fc
agaaaptr <gagah.p.412@gmail.com>
2023-07-26 20:36:33 +0700

    rebase(appversion): Manifest

---
+++ app/src/main/java/com/example/dietproapp/ui/profil/EditProfilActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dietproapp.ui.profil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dietproapp.R
+
+class EditProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profil)
+    }
+}
