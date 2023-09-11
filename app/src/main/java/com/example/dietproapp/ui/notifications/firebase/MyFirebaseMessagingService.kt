d570d428c36412b59e9de8da6d36db620af674c5
aalfa <aalfatah492@gmail.com>
2023-09-11 09:55:34 +0700

    feature(mainfeature): Fitur Search Jurnal Makanan

--- app/src/main/java/com/example/dietproapp/ui/notifications/firebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/dietproapp/ui/notifications/firebase/MyFirebaseMessagingService.kt
@@ -1,138 +1,82 @@
 package com.example.dietproapp.ui.notifications.firebase
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
-import androidx.test.core.app.ApplicationProvider
 import com.example.dietproapp.R
 import com.example.dietproapp.ui.notifications.NotifikasiActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+const val channelId =   "notification_channel"
+const val channelName   =   "com.example.dietproapp"
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MyFirebaseMessagingService : FirebaseMessagingService()  {
 
-class MyFirebaseMessagingService : FirebaseMessagingService() {
-
-    // Override onNewToken to get new token
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-    }
-
-    // Override onMessageReceived() method to extract the
-    // title and
-    // body from the message passed in FCM
+    private val TAG = "MyFirebaseMessaging"
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // First case when notifications are received via
-        // data event
-        // Here, 'title' and 'message' are the assumed names
-        // of JSON
-        // attributes. Since here we do not have any data
-        // payload, This section is commented out. It is
-        // here only for reference purposes.
-        /*if(remoteMessage.getData().size()>0){
-            showNotification(remoteMessage.getData().get("title"),
-                        remoteMessage.getData().get("message"));
-        }*/
 
-        // Second case when notification payload is
-        // received.
-        if (remoteMessage.getNotification() != null) {
-            // Since the notification is received directly from
-            // FCM, the title and the body can be fetched
-            // directly as below.
-            remoteMessage.getNotification()!!.getTitle()?.let {
-                remoteMessage.getNotification()!!.getBody()?.let { it1 ->
-                    showNotification(
-                        it,
-                        it1
-                    )
-                }
-            }
+        Log.d(TAG, "Pesan FCM diterima.")
+
+        if (remoteMessage.notification != null) {
+
+            Log.d(TAG, "Judul Notifikasi: ${remoteMessage.notification!!.title}")
+            Log.d(TAG, "Isi Notifikasi: ${remoteMessage.notification!!.body}")
+
+            generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
         }
+
     }
 
-    // Method to get the custom Design for the display of
-    // notification.
-    private fun getCustomDesign(
-        title: String,
-        message: String,
-    ): RemoteViews {
-        val remoteViews = RemoteViews(
-            ApplicationProvider.getApplicationContext<Context>().getPackageName(),
-            R.layout.notification
-        )
-        remoteViews.setTextViewText(R.id.title, title)
-        remoteViews.setTextViewText(R.id.message, message)
-        remoteViews.setImageViewResource(
-            R.id.appLogo,
-            R.drawable.notif
-        )
-        return remoteViews
-    }
+    @SuppressLint("RemoteViewLayout")
+    fun getRemoteView(title: String, message: String):  RemoteViews{
+        val remoteView = RemoteViews("com.example.dietproapp", R.layout.notification)
 
-    // Method to display the notifications
-    fun showNotification(
-        title: String,
-        message: String,
-    ) {
-        // Pass the intent to switch to the MainActivity
-        val intent = Intent(this, NotifikasiActivity::class.java)
-        // Assign channel ID
-        val channel_id = "notification_channel"
-        // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
-        // the activities present in the activity stack,
-        // on the top of the Activity that is to be launched
+        remoteView.setTextViewText(R.id.title, title)
+        remoteView.setTextViewText(R.id.message, message)
+        remoteView.setImageViewResource(R.id.appLogo, R.drawable.notif)
+
+        return remoteView
+    }
+    private fun generateNotification(title: String, message: String) {
+
+        Log.d(TAG, "Membuat dan menampilkan notifikasi.")
+
+        val intent = Intent(this,NotifikasiActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        // Pass the intent to PendingIntent to start the
-        // next Activity
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent,
-            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
-        )
 
-        // Create a Builder object using NotificationCompat
-        // class. This will allow control over all the flags
-        var builder: NotificationCompat.Builder = NotificationCompat.Builder(
-            ApplicationProvider.getApplicationContext<Context>(),
-            channel_id
-        )
-            .setSmallIcon(R.drawable.notif)
+        val pendingIntent   =   PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+        //channel id, channel name
+        var builder: NotificationCompat.Builder =   NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.dietpro)
             .setAutoCancel(true)
-            .setVibrate(
-                longArrayOf(
-                    1000, 1000, 1000,
-                    1000, 1000
-                )
-            )
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
             .setOnlyAlertOnce(true)
             .setContentIntent(pendingIntent)
 
-        // A customized design for the notification can be
-        // set only for Android versions 4.1 and above. Thus
-        // condition for the same is checked here.
-        builder = builder.setContent(
-            getCustomDesign(title, message)
-        )
-        // Create an object of NotificationManager class to
-        // notify the
-        // user of events that happen in the background.
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
-        // Check if the Android Version is greater than Oreo
-        if (Build.VERSION.SDK_INT
-            >= Build.VERSION_CODES.O
-        ) {
-            val notificationChannel = NotificationChannel(
-                channel_id, "dietproapp",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationManager!!.createNotificationChannel(
-                notificationChannel
-            )
+        builder =   builder.setContent(getRemoteView(title, message))
+
+
+        val notificationManager =   getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationChannel =   NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+
         }
-        notificationManager!!.notify(0, builder.build())
+
+        notificationManager.notify(0,builder.build())
+
     }
+
 }
