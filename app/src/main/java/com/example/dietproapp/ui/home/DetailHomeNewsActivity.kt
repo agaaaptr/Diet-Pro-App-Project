360e4eaa15219e8b1595eb21e499748493546f3d
aalfa <aalfatah492@gmail.com>
2023-09-29 07:00:40 +0700

    feature(mainfeature): corrective HealthNews

--- app/src/main/java/com/example/dietproapp/ui/home/DetailHomeNewsActivity.kt
+++ app/src/main/java/com/example/dietproapp/ui/home/DetailHomeNewsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.dietproapp.ui.home
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dietproapp.databinding.ActivityDetailHomeNewsBinding
@@ -21,27 +23,29 @@ class DetailHomeNewsActivity : AppCompatActivity() {
 
 //        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        //Shimmer Effect
-        binding.webView.visibility = View.GONE
-        binding.shimmerView.stopShimmerAnimation()
+        val toolbar = binding.toolbar
+        val progressBar = binding.progressBar
 
-        Handler().postDelayed({
-            binding.webView.visibility   =   View.VISIBLE
-            binding.shimmerView.stopShimmerAnimation()
-            binding.shimmerView.visibility  =   View.GONE
-        },5000)//seconds
+        setSupportActionBar(toolbar)
+        assert(supportActionBar != null)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
+        progressBar.max = 100
+
+        //Shimmer Effect
+//        binding.webView.visibility = View.GONE
+//        binding.shimmerView.stopShimmerAnimation()
+//
+//        Handler().postDelayed({
+//            binding.webView.visibility   =   View.VISIBLE
+//            binding.shimmerView.stopShimmerAnimation()
+//            binding.shimmerView.visibility  =   View.GONE
+//        },5000)//seconds
         //after 5 seconds loading ui will be show
 
-        val articleUrl = intent.getStringExtra("article_url")
-
-        val webView = binding.webView
-        webView.apply {
-            webViewClient = WebViewClient()
-            if (articleUrl != null) {
-                loadUrl(articleUrl)
-            }
-        }
-
+        showWebView()
         mainButton()
     }
 
@@ -60,4 +64,41 @@ class DetailHomeNewsActivity : AppCompatActivity() {
             startActivity(Intent.createChooser(share, "Bagikan ke : "))
         }
     }
+    private fun showWebView() {
+        var articleUrl = intent.getStringExtra("article_url")
+        val progressBar =   binding.progressBar
+        val webView = binding.webView
+
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.setSupportZoom(true)
+        webView.settings.builtInZoomControls = true
+        webView.settings.displayZoomControls = false
+        webView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        webView.loadUrl(articleUrl!!)
+
+        progressBar.progress = 0
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, newUrl: String): Boolean {
+                view.loadUrl(newUrl)
+                progressBar.progress = 0
+                return true
+            }
+
+            override fun onPageStarted(view: WebView, urlStart: String, favicon: Bitmap?) {
+                super.onPageStarted(view, urlStart, favicon)
+                articleUrl = urlStart
+                invalidateOptionsMenu()
+            }
+
+            override fun onPageFinished(view: WebView, urlPage: String) {
+                super.onPageFinished(view, urlPage)
+                progressBar.visibility = View.GONE
+                invalidateOptionsMenu()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/dietproapp/util/Constants.kt
+++ app/src/main/java/com/example/dietproapp/util/Constants.kt
@@ -2,6 +2,6 @@ package com.example.dietproapp.util
 
 object Constants {
 
-    const val BASE_URL  =   "http://192.168.10.15/dietpro-app/public/"
+    const val BASE_URL  =   "http://192.168.1.15/dietpro-app/public/"
     const val USER_URL =   BASE_URL + "storage/user"
 }
